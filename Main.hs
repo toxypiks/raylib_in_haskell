4176ba0b88a2f265a194eccc833e161febac0618
toxypiks <HannaHinkelbein89@gmx.de>
2025-07-10 19:11:02 +0200

    Implement event loop

--- Main.hs
+++ Main.hs
@@ -1,13 +1,28 @@
 {-# LANGUAGE CApiFFI #-}
 
 import Data.Function
+import Control.Monad
 import Foreign.C.Types
 import Foreign.C.String
 
 foreign import capi "raylib.h InitWindow" initWindow :: CInt -> CInt -> CString -> IO ()
---foreign import capi "raylib.h WindowShouldClose" windowShouldClose :: IO CBool
+foreign import capi "raylib.h WindowShouldClose" windowShouldClose :: IO CBool
+foreign import capi "raylib.h BeginDrawing" beginDrawing :: IO ()
+foreign import capi "raylib.h EndDrawing" endDrawing :: IO ()
+foreign import capi "raylib.h SetTargetFPS" setTargetFPS :: CInt -> IO ()
 
-{--main :: IO ()
+while :: Monad f => f Bool -> f () -> f ()
+while cond body = do
+  x <- cond
+  if x then return ()
+  else do body
+          while cond body
+
+main :: IO ()
 main = do
-  title <- newCstring "Hello from Suskell"
-  initWindow 800 600 title--}
+  title <- newCString "Hello from Suskell"
+  initWindow 800 600 title
+  setTargetFPS 60
+  while ((/= 0) <$> windowShouldClose) $ do
+    beginDrawing
+    endDrawing
--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 `ghci -I./usr/include/ -L./usr/lib/ -lraylib Main.hs`
 
-*open raylib window from ghci:
+open raylib window from ghci:
 
 `newCString "Hello" >>= initWindow 800 600`
+
+compiling Main.hs with ghc
+
+`ghc -I./usr/include/ -L./usr/lib/ -lraylib -dynamic Main.hs`
